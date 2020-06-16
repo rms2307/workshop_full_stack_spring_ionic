4acbe29e9475bc8de1a5f9b1d48fca621725d9db
Robson Silva <rms2307@outlook.com>
2020-06-16 18:50:16 -0300

    Instanciando categorias

--- src/main/java/com/rms2307/ecommerce/EcommerceApplication.java
+++ src/main/java/com/rms2307/ecommerce/EcommerceApplication.java
@@ -1,13 +1,32 @@
 package com.rms2307.ecommerce;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.rms2307.ecommerce.domain.Categoria;
+import com.rms2307.ecommerce.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class EcommerceApplication {
+public class EcommerceApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));		
 	}
 
 }
