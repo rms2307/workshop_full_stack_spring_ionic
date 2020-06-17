e7fd0e0c2d2361c6dc9fa79907c78435f3463108
Robson Silva <rms2307@outlook.com>
2020-06-17 19:34:00 -0300

    Criando metodo POST da Categoria

--- src/main/java/com/rms2307/ecommerce/resources/CategoriaResource.java
+++ src/main/java/com/rms2307/ecommerce/resources/CategoriaResource.java
@@ -1,11 +1,16 @@
 package com.rms2307.ecommerce.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rms2307.ecommerce.domain.Categoria;
 import com.rms2307.ecommerce.services.CategoriaService;
@@ -21,6 +26,13 @@ public class CategoriaResource {
 	public ResponseEntity<Categoria> buscar(@PathVariable Integer id) {
 		Categoria obj = service.bucarPorId(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> inserir(@RequestBody Categoria obj) {
+		obj = service.inserir(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();			
 	}
 
 }
