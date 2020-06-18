77ecea37653b649ce5951f9b2b2a092cd6499a00
Robson Silva <rms2307@outlook.com>
2020-06-18 10:42:23 -0300

    Metodo DELETE Categoria

--- src/main/java/com/rms2307/ecommerce/services/CategoriaService.java
+++ src/main/java/com/rms2307/ecommerce/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.rms2307.ecommerce.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.rms2307.ecommerce.domain.Categoria;
 import com.rms2307.ecommerce.repositories.CategoriaRepository;
+import com.rms2307.ecommerce.services.exceptions.DataIntegrityException;
 import com.rms2307.ecommerce.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -20,14 +22,24 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		findById(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		findById(id);
+
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Categoria com produtos");
+		}
 	}
 }
