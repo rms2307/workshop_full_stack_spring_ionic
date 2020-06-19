bcb5fd3cb919b1f8bc242ae24db37df2e0651a9b
Robson Silva <rms2307@outlook.com>
2020-06-19 16:21:50 -0300

    Validação customizada: não permitindo email já cadastrado

--- src/main/java/com/rms2307/ecommerce/repositories/ClienteRepository.java
+++ src/main/java/com/rms2307/ecommerce/repositories/ClienteRepository.java
@@ -1,11 +1,16 @@
 package com.rms2307.ecommerce.repositories;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.rms2307.ecommerce.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String emai);
+	
 }
--- src/main/java/com/rms2307/ecommerce/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/rms2307/ecommerce/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.rms2307.ecommerce.domain.Cliente;
 import com.rms2307.ecommerce.domain.enums.TipoCliente;
 import com.rms2307.ecommerce.dto.ClienteNewDTO;
+import com.rms2307.ecommerce.repositories.ClienteRepository;
 import com.rms2307.ecommerce.resources.exceptions.FieldMessage;
 import com.rms2307.ecommerce.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+	@Autowired
+	private ClienteRepository repo;
+
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -19,15 +27,20 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 	@Override
 	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
 		List<FieldMessage> list = new ArrayList<>();
-		
-		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CPF Inválido"));
 		}
-		
-		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido"));
 		}
-		
+
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já cadastrado"));
+		}
+
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
