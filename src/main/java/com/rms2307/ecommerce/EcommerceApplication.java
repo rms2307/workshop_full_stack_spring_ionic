2cb2c2845eaf400ba44d30dca94fa3e64498a1cf
Robson Silva <rms2307@outlook.com>
2020-06-24 15:53:46 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/rms2307/ecommerce/EcommerceApplication.java
+++ src/main/java/com/rms2307/ecommerce/EcommerceApplication.java
@@ -1,19 +1,25 @@
 package com.rms2307.ecommerce;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.rms2307.ecommerce.services.S3Service;
+
 @SpringBootApplication
 public class EcommerceApplication implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		s3Service.uploadFile("G:\\001.jpg");
 	}
 
 }
